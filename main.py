51885939ec3c5e9f52a7f14ff9f0f301a4c4df3d
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-11-30 22:56:43 -0200

    Gender and class model like tutorial

--- main.py
+++ main.py
@@ -50,3 +50,66 @@ for row in test_file_object:
 
 test_file.close()
 prediction_file.close()
+
+fare_ceiling = 40
+
+data[data[0::, 9].astype(np.float) >= fare_ceiling, 9] = fare_ceiling - 1
+
+fare_bracket_size = 10
+number_of_price_brackets = int(fare_ceiling / fare_bracket_size)
+
+number_of_classes = len(np.unique(data[0::, 2]))
+
+survivor_table = np.zeros((2, number_of_classes, number_of_price_brackets))
+
+for i in range(number_of_classes):
+    for j in range(number_of_price_brackets):
+        women_only = data[
+            (data[0::, 4] == "female") & (data[0::, 2].astype(np.float) == i + 1) & (
+                data[0:, 9].astype(np.float) >= j * fare_bracket_size) & (
+                data[0:, 9].astype(np.float) < (j + 1) * fare_bracket_size), 1
+        ]
+
+        men_only = data[
+            (data[0::, 4] != "female") & (data[0::, 2].astype(np.float) == i + 1) & (
+                data[0:, 9].astype(np.float) >= j * fare_bracket_size) & (
+                data[0:, 9].astype(np.float) < (j + 1) * fare_bracket_size), 1
+        ]
+        survivor_table[0, i, j] = np.mean(women_only.astype(np.float))
+        survivor_table[1, i, j] = np.mean(men_only.astype(np.float))
+
+        survivor_table[survivor_table != survivor_table] = 0
+
+        survivor_table[survivor_table < 0.5] = 0
+        survivor_table[survivor_table >= 0.5] = 1
+
+print(survivor_table)
+
+test_file = open('test.csv', 'r')
+test_file_object = csv.reader(test_file)
+test_file_object.__next__()
+
+predictions_file = open("gender_class_model.csv", "w", newline='')
+p = csv.DictWriter(predictions_file, fieldnames=["PassengerId", "Survived"])
+p.writeheader()
+
+for row in test_file_object:
+    for j in range(number_of_price_brackets):
+        try:
+            row[8] = float(row[8])
+        except:
+            bin_fare = 3 - float(row[1])
+            break
+        if row[8] > fare_ceiling:
+            bin_fare = number_of_price_brackets - 1
+            break
+        if j * fare_bracket_size <= row[8] < (j + 1) * fare_bracket_size:
+            bin_fare = j
+            break
+    if row[3] == 'female':  # If the passenger is female
+        p.writerow({'PassengerId': row[0], 'Survived': int(survivor_table[0, int(row[1]) - 1, bin_fare])})
+    else:  # else if male
+        p.writerow({'PassengerId': row[0], 'Survived': int(survivor_table[1, int(row[1]) - 1, bin_fare])})
+
+test_file.close()
+predictions_file.close()
